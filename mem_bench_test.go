a995c2866c7d4db802245f2b6e75700279ff8bed
NikoMalik <123812634+NikoMalik@users.noreply.github.com>
2024-12-02 22:03:11 +0300

    change pool impl

--- mem_bench_test.go
+++ mem_bench_test.go
@@ -3,26 +3,12 @@ package jwt
 import (
 	"sync"
 	"testing"
+
+	"github.com/NikoMalik/mutex"
 )
 
 type MyObject struct {
 	ID int
-}
-
-func BenchmarkCustomPool(b *testing.B) {
-	pool := newObjPool[MyObject](1024, 128, func() MyObject {
-
-		return MyObject{}
-	})
-	b.ResetTimer()
-
-	b.Run("Get and Put", func(b *testing.B) {
-		obj := pool.get()
-		for i := 0; i < b.N; i++ {
-			obj.ID = i
-			pool.put(obj)
-		}
-	})
 }
 
 func BenchmarkSyncPool(b *testing.B) {
@@ -35,10 +21,75 @@ func BenchmarkSyncPool(b *testing.B) {
 	b.ResetTimer()
 
 	b.Run("Get and Put", func(b *testing.B) {
+		obj := pool.Get().(MyObject)
 		for i := 0; i < b.N; i++ {
-			obj := pool.Get().(MyObject)
 			obj.ID = i
 			pool.Put(obj)
+		}
+	})
+
+	b.Run("Get and Put async", func(b *testing.B) {
+		obj := pool.Get().(MyObject)
+		for i := 0; i < b.N; i++ {
+			go func() {
+				obj.ID = i
+				pool.Put(obj)
+			}()
+		}
+	})
+}
+
+func BenchmarkPoolChan(b *testing.B) {
+	pool := NewPoolChan[MyObject](b.N, func() MyObject {
+
+		return MyObject{}
+
+	})
+	b.ResetTimer()
+
+	b.Run("Get and Put", func(b *testing.B) {
+		obj := pool.Get()
+		for i := 0; i < b.N; i++ {
+			obj.ID = i
+			pool.Put(obj)
+		}
+	})
+
+	b.Run("Get and Put async", func(b *testing.B) {
+		obj := pool.Get()
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				obj.ID = i
+				pool.Put(obj)
+			}(i)
+		}
+	})
+}
+
+func BenchmarkOldPool(b *testing.B) {
+	old := nObjPool[MyObject](b.N, func() MyObject {
+
+		return MyObject{}
+	})
+
+	b.ResetTimer()
+	b.Run("Get and Put", func(b *testing.B) {
+		obj := old.Get()
+		for i := 0; i < b.N; i++ {
+			obj.ID = i
+			old.Put(obj)
+		}
+
+	})
+	old.mut = &mutex.MutexExp{}
+
+	b.Run("Get and Put async", func(b *testing.B) {
+		obj := old.Get()
+		for i := 0; i < b.N; i++ {
+			go func(i int) {
+				obj.ID = i
+				old.Put(obj)
+			}(i)
 		}
 	})
 }
--- mem_pool.go
+++ mem_pool.go
@@ -1,9 +1,11 @@
 package jwt
 
 import (
+	"sync/atomic"
 	"unsafe"
 
-	lowlevelfunctions "github.com/NikoMalik/low-level-functions"
+	"github.com/NikoMalik/low-level-functions/constants"
+	"github.com/NikoMalik/mutex"
 )
 
 const (
@@ -25,178 +27,127 @@ const (
 	size_pool = 0x04
 )
 
-type objectPool[T any] struct {
-	// 	"github.com/NikoMalik/low-level-functions/constants"
-	_          noCopy
-	obj        [][]T
-	allocate   func() T
-	freeptr    []int32
-	currOffset int32
-	currChunk  int32
-	chunkSize  int32
-	cap        int32
-	_          lowlevelfunctions.CacheLinePadding
+type PoolChan[T any] struct {
+	_ noCopy
+	c [1]atomic.Pointer[T]
+
+	new func() T
+	_   [constants.CacheLinePadSize - unsafe.Sizeof(func() any { return nil })]byte
 }
 
-func (p *objectPool[T]) _t_(ptr uintptr) *T {
-	if ptr == 0 {
-		panic("nil pointer dereference")
+func NewPoolChan[T any](size int, new func() T) *PoolChan[T] {
+	if size <= 0 {
+		panic("size must be greater than 0")
 	}
-	return (*T)(unsafe.Pointer(ptr))
-}
-
-func (p *objectPool[T]) _u_(v *T) uintptr {
-	if v == nil {
-		panic("nil pointer dereference")
+	pool := &PoolChan[T]{
+		c:   [1]atomic.Pointer[T]{},
+		new: new,
 	}
-	return uintptr(unsafe.Pointer(v))
-}
-
-func (p *objectPool[T]) malloc(numChunks int32) {
-	if numChunks <= 0 {
-		numChunks = 1
-	}
-
-	for chunk := int32(0); chunk < numChunks; chunk++ {
-		newChunk := make([]T, p.chunkSize)
-
-		for i := 0; i < len(newChunk); i += 4 {
-			newChunk[i] = p.allocate()
-			if i+1 < len(newChunk) {
-				newChunk[i+1] = p.allocate()
-			}
-			if i+2 < len(newChunk) {
-				newChunk[i+2] = p.allocate()
-			}
-			if i+3 < len(newChunk) {
-				newChunk[i+3] = p.allocate()
-			}
-		}
-		p.obj = append(p.obj, newChunk)
-	}
-}
-
-func newObjPool[T any](cap, chunkSize int32, f func() T) *objectPool[T] {
-	if cap <= 0 {
-		cap = 1024 // default capacity
-	}
-	if chunkSize <= 0 {
-		chunkSize = 128 // default chunk size
-	}
-
-	obj := make([][]T, 0, cap)
-	for i := int32(0); i < cap; i++ {
-		obj = append(obj, alignGeneric[T](int(chunkSize), 32))
-	}
-
-	return &objectPool[T]{
-		allocate:   f,
-		chunkSize:  chunkSize,
-		cap:        cap,
-		obj:        obj,
-		freeptr:    make([]int32, 0, cap),
-		currOffset: 0,
-		currChunk:  0,
-	}
-
-}
-
-func (o *objectPool[T]) get() T {
-	if len(o.freeptr) > 0 {
-		idx := o.freeptr[len(o.freeptr)-1]
-		o.freeptr = o.freeptr[:len(o.freeptr)-1]
-
-		chunk := idx / o.chunkSize
-		offset := idx % o.chunkSize
-
-		if chunk < 0 || chunk >= int32(len(o.obj)) || offset < 0 || offset >= o.chunkSize {
-			panic("index out of bounds in object pool")
-		}
-
-		return o.obj[chunk][offset]
-	}
-
-	if len(o.obj) == 0 || o.currOffset == o.chunkSize {
-		o.malloc(1)
-		o.currOffset = 0
-		o.currChunk = int32(len(o.obj)) - 1
-	}
-
-	if o.currChunk < 0 || o.currChunk >= int32(len(o.obj)) || o.currOffset < 0 || o.currOffset >= o.chunkSize {
-		panic("index out of bounds in object pool")
-	}
-
-	obj := o.allocate()
-	o.obj[o.currChunk][o.currOffset] = obj
-	o.currOffset++
-	return obj
-}
-
-func (p *objectPool[T]) put(obj T) {
-	if int32(len(p.freeptr)) >= p.cap {
-		return
-	}
-
-	chunk := p.currChunk
-	offset := p.currOffset - 1
-
-	if chunk < 0 || chunk >= int32(len(p.obj)) || offset < 0 || offset >= p.chunkSize {
-		panic("index out of bounds in object pool")
-	}
-
-	p.freeptr = append(p.freeptr, chunk*p.chunkSize+offset)
-}
-
-type _oldObjectPool[T any] struct {
-	_        noCopy
-	obj      []T
-	allocate func() T
-	cap      int
-}
-
-func oldObjPool[T any](cap int, f func() T) *_oldObjectPool[T] {
-	pool := &_oldObjectPool[T]{allocate: f}
-
-	if cap < 0 {
-		return nil
-	}
-	if cap <= 0 || cap > MAX_SIZE {
-		cap = MAX_SIZE // Cap to a reasonable maximum size
-	}
-
-	if cap <= 1 {
-		pool.cap = 0x0FFFFFFF
-		pool.obj = make([]T, 0, KB)
-		// sht := (*reflect.SliceHeader)(unsafe.Pointer(&pool.obj))
-		// sht.Cap = KB
-		// sht.Len = 0
-		// sht.Data = uintptr(unsafe.Pointer(&j))
-
-	} else {
-		pool.cap = cap
-		pool.obj = make([]T, 0, cap)
-		// sht := (*reflect.SliceHeader)(unsafe.Pointer(&pool.obj))
-
-	}
+	// for i := 0; i < size; i++ {
+	// 	pool.c[i].Store(nil)
+	// }
 
 	return pool
 }
 
-func (o *_oldObjectPool[T]) get() T {
+func (p *PoolChan[T]) Get() T {
+	for i := uint32(0); i < 1; i++ {
+		ptr := p.c[i].Load()
+		if ptr != nil {
 
-	if len(o.obj) > 0 {
+			if p.c[i].CompareAndSwap(ptr, nil) {
+				return *ptr
+			}
+		}
+	}
+
+	return p.new()
+}
+
+func (p *PoolChan[T]) Put(obj T) {
+	for i := uint32(0); i < 1; i++ {
+		if p.c[i].CompareAndSwap(nil, &obj) {
+			return
+		}
+	}
+}
+
+func (p *PoolChan[T]) CurrentSize() int {
+	size := 0
+	for i := uint32(0); i < 1; i++ {
+		ptr := p.c[i].Load()
+		if ptr != nil {
+			size++
+		}
+	}
+	return size
+}
+
+type objPool[T any] struct {
+	mut      *mutex.MutexExp
+	_        [constants.CacheLinePadSize - 8]byte
+	obj      []T
+	_        [constants.CacheLinePadSize - unsafe.Sizeof([]T{})]byte
+	allocate func() T
+	_        [constants.CacheLinePadSize - unsafe.Sizeof(func() any { return nil })]byte
+	size     int
+	_        [constants.CacheLinePadSize - unsafe.Sizeof(int(0))]byte
+}
+
+func nObjPool[T any](capt int, f func() T) *objPool[T] {
+
+	pool := &objPool[T]{allocate: f}
+
+	if capt < 0 {
+		return nil
+	}
+
+	pool.size = capt
+	pool.obj = make([]T, 0, capt)
+
+	return pool
+}
+
+func (o *objPool[T]) Get() T {
+	if len(o.obj) <= 0 {
+		return o.allocate()
+
+	}
+	if o.mut == nil {
 
 		obj := o.obj[len(o.obj)-1]
 		o.obj = o.obj[:len(o.obj)-1]
 		return obj
+
 	}
-	return o.allocate() // Create a new object if pool is empty
+	if o.mut != nil {
+		o.mut.Lock()
+		obj := o.obj[len(o.obj)-1]
+		o.obj = o.obj[:len(o.obj)-1]
+
+		o.mut.Unlock()
+		return obj
+	}
+	var zero T
+	return zero
+
 }
-func (o *_oldObjectPool[T]) put(obj T) {
-	if len(o.obj) < o.cap {
 
-		o.obj = append(o.obj, obj)
+func (o *objPool[T]) Put(obj T) {
+	if o.mut != nil {
+		o.mut.Lock()
+		if len(o.obj) < o.size {
+
+			o.obj = append(o.obj, obj)
+		}
+
+		o.mut.Unlock()
 	}
-	return
+	if o.mut == nil {
 
+		if len(o.obj) < o.size {
+
+			o.obj = append(o.obj, obj)
+		}
+	}
 }
